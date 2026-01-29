5424e230de63963f5089a0ec52b5bd39bc818231
shovontanvir <shovon.tanvir.91@gmail.com>
2026-01-29 15:07:15 +0600

    Add overflow-x-hidden class to RootLayout for improved layout handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${ppNeueMontreal.variable} antialiased`}
+        className={`${inter.variable} ${ppNeueMontreal.variable} antialiased overflow-x-hidden`}
       >
         {children}
       </body>
